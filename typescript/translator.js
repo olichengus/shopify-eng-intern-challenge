ae790ae6457e85cfdb0756ddb2a826b6124dd41e
olicheng <olicheng@student.ubc.ca>
2024-09-07 20:45:02 -0700

    Added stub

---
+++ typescript/translator.js
@@ -0,0 +1,6 @@
+var BRAILLE_TO_LETTERS = {};
+var BRAILLE_TO_NUMBERS = {};
+var BRAILLE_TO_SPECIAL_CHAR = {};
+var inputString = process.argv[2];
+var res = inputString;
+console.log(res);
---
+++ typescript/translator.ts
@@ -0,0 +1,18 @@
+const BRAILLE_TO_LETTERS = {
+
+}
+
+const BRAILLE_TO_NUMBERS = {
+
+}
+
+const BRAILLE_TO_SPECIAL_CHAR = {
+
+}
+
+const inputString: string = process.argv[2];
+
+const res : string = inputString;
+
+console.log(res);
+
